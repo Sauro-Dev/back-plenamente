907eddc68cbf0bf73e7e2016e5f50b518da411eb
DinoBudino <zahir_mauricio@outlook.es>
2024-08-06 19:05:42 -0500

    Dependencias generadas

---
+++ src/test/java/com/plenamente/sgt/SgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plenamente.sgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
