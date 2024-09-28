acc21f664ab682e53f808a0271d703e03318b699
FluidityCore <Bertlas.u@gmail.com>
2024-09-28 18:25:10 -0500

    Creacion de Service y ServiceImpl
    Para Registrar ambiente

---
+++ src/main/java/com/plenamente/sgt/service/RoomService.java
@@ -0,0 +1,9 @@
+package com.plenamente.sgt.service;
+
+import com.plenamente.sgt.domain.entity.Room;
+
+import java.util.List;
+
+public interface RoomService {
+    Room registerRoom(Room room);
+}
---
+++ src/main/java/com/plenamente/sgt/service/impl/RoomServiceImpl.java
@@ -0,0 +1,28 @@
+package com.plenamente.sgt.service.impl;
+
+import com.plenamente.sgt.domain.entity.Room;
+import com.plenamente.sgt.infra.repository.RoomRepository;
+import com.plenamente.sgt.service.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RoomServiceImpl implements RoomService {
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Override
+    public Room registerRoom(Room room) {
+        if (room.getName() == null || room.getName().isEmpty()) {
+            throw new IllegalArgumentException("Name is required");
+        }
+        if (room.getAddress() == null || room.getAddress().isEmpty()) {
+            throw new IllegalArgumentException("Address is required");
+        }
+        return roomRepository.save(room);
+    }
+}
+
