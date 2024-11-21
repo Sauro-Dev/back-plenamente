e8dd764f58197a18ba56cf0b3154e77fe8c76c88
jhoneiro <jhoneiro12@hotmail.com>
2024-11-21 12:21:35 -0500

    cambios minimos insignificantes

--- src/main/java/com/plenamente/sgt/infra/repository/MaterialRepository.java
+++ src/main/java/com/plenamente/sgt/infra/repository/MaterialRepository.java
@@ -3,12 +3,10 @@ package com.plenamente.sgt.infra.repository;
 import com.plenamente.sgt.domain.entity.Material;
 import com.plenamente.sgt.domain.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface MaterialRepository extends JpaRepository<Material, String> {
     Optional<Material> findTopByOrderByIdMaterialDesc();
     List<Material> findByRoomIsNull();
--- src/main/java/com/plenamente/sgt/service/impl/UserServiceImpl.java
+++ src/main/java/com/plenamente/sgt/service/impl/UserServiceImpl.java
@@ -24,6 +24,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
